3cbfb3abe3f0be7d008ca4c0b038f8c53bb059e7
alireza <alirezamolaii@gmail.com>
2019-05-20 13:35:36 +0430

    add api doc

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,9 +2,14 @@ import express from 'express';
 import userRoutes from './user';
 import eventRoutes from './event';
 import categoriesRoutes from './categories';
+import YAML from 'yamljs';
+import swaggerUi from 'swagger-ui-express';
+
+const swaggerDocument = YAML.load('./openapi.yaml');
 
 const router = express.Router();
 
+router.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.use('/user', userRoutes);
 router.use('/event', eventRoutes);
 router.use('/category', categoriesRoutes);
